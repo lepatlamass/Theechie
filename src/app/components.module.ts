4f719cd6d7b8a13317de65ae29af71318dc72153
lepatlamass <kwonmazoukonwo@gmail.com>
2020-07-29 21:35:03 +0100

    footer updated

--- src/app/components.module.ts
+++ src/app/components.module.ts
@@ -3,10 +3,11 @@ import { IonicModule } from '@ionic/angular';
 import { CommonModule } from '@angular/common';
 import { Routes, RouterModule, Router } from '@angular/router';
 import { NavComponent } from './nav/nav.component';
+import { FooterComponent } from './footer/footer.component';
 
 @NgModule ({
-    declarations: [ NavComponent ],
-    exports: [NavComponent],
+    declarations: [ NavComponent,FooterComponent ],
+    exports: [NavComponent,FooterComponent],
     imports: [RouterModule]
 })
 export class ComponentsModule{}
