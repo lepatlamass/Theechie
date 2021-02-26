c255374d771bea27b5339d09d28a6f65c7d381c0
lepatlamass <kwonmazoukonwo@gmail.com>
2021-02-26 10:58:50 +0100

    added somethings

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,10 +3,19 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false,
+  production: true,
   contentfuel: {
     spaceId:'wej9aqf7u9yq',
     token:'FfZDKBBcF7QOoCI6D_d1qo3XKXklXzkhyVTqwWs07_8'
+  },
+  firebaseConfig: {
+    apiKey: "AIzaSyBGDJX7c6F6EBXtAzBEpo_ttDw6Ntk3alI",
+    authDomain: "thetekkie-c2537.firebaseapp.com",
+    projectId: "thetekkie-c2537",
+    storageBucket: "thetekkie-c2537.appspot.com",
+    messagingSenderId: "386954167822",
+    appId: "1:386954167822:web:25668b412e23d4bdcb1e5f",
+    measurementId: "G-G9SZTYMHWX"
   }
 };
 
